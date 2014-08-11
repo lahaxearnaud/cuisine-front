83145b65c4281881bff6ae081bb3a6d053dbfa7b
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2014-08-11 15:21:15 +0200

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 // cuisine-front - created with Gulp Fiction
-var gulp = require("gulp");
-var concat = require("gulp-concat");
-var cssshrink = require("gulp-cssshrink");
-var notify = require("gulp-notify");
-var connect = require('gulp-connect');
+var gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    cssshrink = require("gulp-cssshrink"),
+    notify = require("gulp-notify"),
+    connect = require('gulp-connect'),
+    connect = require('gulp-angular-filesort'),
+    inject = require("gulp-inject"),
+    angularFilesort = require('gulp-angular-filesort');
 
-gulp.task("js", [], function () {
-    gulp.src([
+/**
+ * [
         "app/js/app.js",
         "app/js/services.js",
         "app/js/controllers.js",
@@ -14,26 +17,45 @@ gulp.task("js", [], function () {
         "app/js/directives.js",
         "app/js/restangular.js",
         "app/js/controllers/*.js"
-    ])
+    ]
+ */
+
+gulp.task("js", ["inject"], function () {
+    gulp.src("app/js/**/*.js")
+        .pipe(angularFilesort())
         .pipe(concat("app.js"))
         .pipe(gulp.dest("./build/"))
-        .pipe(notify("Js done !"))
-        .pipe(connect.reload());
-    ;
+        .pipe(notify("Js done !"));
 });
 
-gulp.task("css", [], function () {
+gulp.task("css", ["inject"], function () {
     gulp.src(["app/css/**.css"])
         .pipe(cssshrink())
         .pipe(concat("app.css"))
         .pipe(gulp.dest("./build/"))
-        .pipe(notify("CSS done !"))
-        .pipe(connect.reload());
+        .pipe(notify("CSS done !"));
 });
 
 gulp.task("html", [], function () {
     gulp.src(["app/**/*.html"])
         .pipe(connect.reload());
+});
+
+gulp.task('inject', function () {
+  var target = gulp.src('app/index.html');
+  var sources = gulp.src([
+    './bower_components/angular/angular.js',
+    './bower_components/angular-route/angular-route.js',
+    './bower_components/lodash/dist/lodash.js',
+    './bower_components/restangular/dist/restangular.js',
+    './build/app.js',
+    './bower_components/bootstrap/dist/css/bootstrap.css',
+    './build/app.css'
+    ], {read: false});
+
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('app/index.html'))
+    .pipe(connect.reload());
 });
 
 gulp.task("watch", [], function () {
