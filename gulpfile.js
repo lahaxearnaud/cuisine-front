72920271b099326affc798aee992b3b373fd1034
LAHAXE Arnaud <Arnaud.LAHAXE.ext@boursorama.fr>
2014-08-13 14:16:39 +0200

    code formater, automation of bower install, log... good stuff =)

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,17 @@ var gulp = require("gulp"),
     connect = require('gulp-connect'),
     inject = require("gulp-inject"),
     imagemin = require('gulp-imagemin'),
-    rewriteCSS = require('gulp-rewrite-css');
+    rewriteCSS = require('gulp-rewrite-css')
+    bower = require('gulp-bower'),
+    prettify = require('gulp-jsbeautifier');
 
 
+
+/**
+ * =============================================
+ *  Configs
+ * =============================================
+ */
 var bowerJsToIntegrate = [
     'bower_components/angular/angular.js',
     'bower_components/angular-route/angular-route.js',
@@ -39,7 +47,107 @@ var appFiles =  [
         "app/js/config/*.js"
     ];
 
-gulp.task("bower", [], function () {
+var logWatch = function(event) {
+  console.log('File '+event.path+' was '+event.type);
+};
+
+
+/**
+ * =============================================
+ *  HTML
+ * =============================================
+ */
+gulp.task("html", [], function () {
+    gulp.src(["app/**/*.html"])
+        .pipe(connect.reload());
+});
+
+/**
+ * =============================================
+ *  Beautifier
+ * =============================================
+ */
+gulp.task('format:js', function() {
+  gulp.src(appFiles)
+    .pipe(prettify({config: '.jsbeautifyrc'}))
+    .pipe(gulp.dest('./app'));
+});
+
+gulp.task('format:html', function() {
+  gulp.src(['app/index.html', 'app/partials/**.html'])
+    .pipe(prettify({config: '.jsbeautifyrc'}))
+    .pipe(gulp.dest('./app'));
+});
+
+gulp.task('format:css', function() {
+  gulp.src(["app/css/**.css"])
+    .pipe(prettify({config: '.jsbeautifyrc'}))
+    .pipe(gulp.dest('./app'));
+});
+
+/**
+ * =============================================
+ *  Assets
+ * =============================================
+ */
+gulp.task("assets:js", [], function () {
+    gulp.src(appFiles)
+        .pipe(concat("app.js"))
+        .pipe(gulp.dest(buildPath))
+        .pipe(notify("Js done !"))
+        .pipe(connect.reload());
+});
+
+gulp.task("assets:css", [], function () {
+    gulp.src(["app/css/**.css"])
+        .pipe(rewriteCSS({
+            destination:buildPath
+        }))
+        .pipe(cssshrink())
+        .pipe(concat("app.css"))
+        .pipe(gulp.dest(buildPath))
+        .pipe(notify("CSS done !"))
+        .pipe(connect.reload());
+});
+
+gulp.task("assets:img", [], function () {
+    gulp.src(["app/**/*.(png|jpg)"])
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/img'))
+        .pipe(notify("Image done !"));
+});
+
+/**
+ * =============================================
+ *  Injector
+ * =============================================
+ */
+
+gulp.task('inject', function () {
+  var target = gulp.src(mainHtmlFile);
+  var sources = gulp.src([
+    './build/libs.**',
+    './build/app.js',
+    './build/libs.css',
+    './build/app.css'
+    ], {read: false});
+
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./app/'))
+    .pipe(connect.reload());
+});
+
+/**
+ * =============================================
+ *  BOWER
+ * =============================================
+ */
+
+gulp.task('bower:install', function() {
+  return bower("./bower_components");
+});
+
+gulp.task("bower:build", [], function () {
     gulp.src(bowerJsToIntegrate)
         .pipe(concat("libs.js"))
         .pipe(gulp.dest(buildPath));
@@ -53,66 +161,36 @@ gulp.task("bower", [], function () {
         .pipe(gulp.dest(buildPath));
 });
 
-gulp.task("js", [], function () {
-    gulp.src(appFiles)
-        .pipe(concat("app.js"))
-        .pipe(gulp.dest(buildPath))
-        .pipe(notify("Js done !"))
-        .pipe(connect.reload());
-});
-
-gulp.task("css", [], function () {
-    gulp.src(["app/css/**.css"])
-        .pipe(rewriteCSS({
-            destination:buildPath
-        }))
-        .pipe(cssshrink())
-        .pipe(concat("app.css"))
-        .pipe(gulp.dest(buildPath))
-        .pipe(notify("CSS done !"))
-        .pipe(connect.reload());
-});
-
-gulp.task("html", [], function () {
-    gulp.src(["app/**/*.html"])
-        .pipe(connect.reload());
-});
-
-gulp.task("img", [], function () {
-    gulp.src(["app/**/*.(png|jpg)"])
-        .pipe(imagemin())
-        .pipe(gulp.dest('./build/img'))
-        .pipe(notify("Image done !"));
-});
-
-
-gulp.task('inject', function () {
-  var target = gulp.src(mainHtmlFile);
-  var sources = gulp.src([
-    './build/libs.js',
-    './build/app.js',
-    './build/libs.css',
-    './build/app.css'
-    ], {read: false});
-
-  return target.pipe(inject(sources))
-    .pipe(gulp.dest('./app/'))
-    .pipe(connect.reload());
-});
-
-gulp.task("watch", [], function () {
-    gulp.watch("app/js/**/*.js", ["js"]);
-    gulp.watch("app/css/**/*.css", ["css"]);
-    gulp.watch("app/**/*.html", ["html"]);
-    gulp.watch("app/img/**/*.(png|jpg)", ["img"]);
-    gulp.watch(["bower_components/**/*.js", "bower_components/**/*.css"], ["bower"]);
-});
-
-gulp.task('webserver', ["bower", "js", "css", "img", "inject"],function () {
+/**
+ * =============================================
+ *  Server
+ * =============================================
+ */
+gulp.task('webserver', ["bower:install", "bower:build", "assets:js", "assets:css", "assets:img", "inject"], function () {
     connect.server({
         livereload: true,
         port: 3333
     });
 });
 
+
+/**
+ * =============================================
+ *  Watchers
+ * =============================================
+ */
+gulp.task("watch", [], function () {
+    gulp.watch("app/js/**/*.js", ["format:js", "assets:js"], logWatch);
+    gulp.watch("app/css/**/*.css", ["format:css", "assets:css"], logWatch);
+    gulp.watch("app/**/*.html", ["format:html", "html"], logWatch);
+    gulp.watch("app/img/**/*.(png|jpg)", ["assets:img"], logWatch);
+    gulp.watch(["bower_components/**/*.js", "bower_components/**/*.css"], ["bower:build"], logWatch);
+    gulp.watch(["bower.json"], ["bower:install"], logWatch);
+});
+
+/**
+ * =============================================
+ *  tasks
+ * =============================================
+ */
 gulp.task("default", ["watch", "webserver"]);
