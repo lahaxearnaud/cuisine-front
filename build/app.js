f87f01fb2fdf3824e931d0e8efe94947539fdf9c
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2014-09-09 08:32:56 +0200

    update category

--- build/app.js
+++ build/app.js
@@ -282,6 +282,17 @@ app.run(['loader', '$rootScope', '$location', 'baseUrl', function(loader, $rootS
 			var element = $rootScope.categories[key];
 			if(element != null && element.id === id) {
 				$rootScope.categories[key] = category;
+				break;
+        	}
+        }
+    };
+
+    $rootScope.removeCategory = function(id) {
+    	for (var key in $rootScope.categories){
+			var element = $rootScope.categories[key];
+			if(element != null && element.id === id) {
+				delete $rootScope.categories[key];
+				break;
         	}
         }
     };
